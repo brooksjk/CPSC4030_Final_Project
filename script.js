6f7f2882083dc9e48b17ec5c932ad32f97d46427
evkes <evkessler2002@gmail.com>
2023-10-26 13:25:54 -0400

    added cleaned crash_data

--- script.js
+++ script.js
@@ -1,81 +1,179 @@
-d3.csv("deadly_crash_data.csv").then(function (dataset) {
+d3.csv("cleaned_crash_data_zipc.csv").then(data => {
+
     var dimensions = {
-        width: 800,
-        height: 800,
-        margin: {
-            top: 10,
-            bottom: 50,
-            right: 10,
-            left: 50
+      svgWidth: 800,
+      svgHeight: 700,
+      mapFitWidth: 800,
+      mapFitHeight: 600,
+      legendX: 10,
+      legendY: 615,
+      legendWidth: 200,
+      legendHeight: 20,
+    };
+
+    let boroughCounts = {};
+
+    data.forEach(row => {
+        let borough = row["BOROUGH"];
+        if (boroughCounts[borough]) {
+            boroughCounts[borough]++;
+        } else {
+            boroughCounts[borough] = 1;
         }
-    }  
+    });
 
-    var xAccessor = d => +d.year
-    
-    var svg = d3.select("#barchart")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
-  
-    var xScale = d3.scaleLinear()
-      .domain(d3.extent(dataset,xAccessor))
-      .range([dimensions.margin.left,dimensions.width-dimensions.margin.right])
+    d3.json("Borough_Boundaries.geojson").then(geoData => {
+        const svg = d3.select("#boroughs")
+            .attr("width", dimensions.svgWidth)
+            .attr("height", dimensions.svgHeight);
 
-    
-    var keys = dataset.columns.slice(1)
+        const projection = d3.geoMercator().fitSize([dimensions.mapFitWidth, dimensions.mapFitHeight], geoData);
+        const path = d3.geoPath().projection(projection);
 
-    var maxl = d3.max(dataset, function(d) {
-      var maxNames = 0
-      for (var i = 0; i < keys.length; i++) {
-        maxNames = maxNames + parseInt(d[keys[i]])
+        const maxCount = 500000;
+
+        const colorScale = d3.scaleLinear()
+            .domain([0, maxCount])
+            .range(["#ffffff", "#013220"]); 
+
+        svg.selectAll("path")
+            .data(geoData.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("fill", d => {
+                const count = boroughCounts[d.properties.boro_name];
+                return colorScale(count);
+            })
+            .attr("stroke", "#000");
+
+        const gradient = svg.append("defs")
+            .append("linearGradient")
+            .attr("id", "gradient");
+
+        gradient.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", "#ffffff")
+            .attr("stop-opacity", 1);
+
+        gradient.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", "#013220")
+            .attr("stop-opacity", 1);
+
+        svg.append("rect")
+            .attr("x", dimensions.legendX)
+            .attr("y", dimensions.legendY)
+            .attr("width", dimensions.legendWidth)
+            .attr("height", dimensions.legendHeight)
+            .style("fill", "url(#gradient)");
+
+        svg.append("text")
+            .attr("x", dimensions.legendX)
+            .attr("y", dimensions.legendY - 5)
+            .style("font-size", "16px")
+            .text("0");
+
+        svg.append("text")
+            .attr("x", dimensions.legendX + dimensions.legendWidth)
+            .attr("y", dimensions.legendY - 5)
+            .style("font-size", "16px")
+            .attr("text-anchor", "end")
+            .text(maxCount);
+    });
+});
+
+d3.csv("cleaned_crash_data_zipc.csv").then(data => {
+  var dimensions = {
+    svgWidth: 800,
+    svgHeight: 700,
+    margin: {
+        top: 100,
+        right: 75,
+        bottom: 75,
+        left: 75
+    }
+  };
+
+  let timeCounts = {};
+
+  data.forEach(row => {
+      let time = row["CRASH TIME"].split(":")[0]
+      if (timeCounts[time]) {
+          timeCounts[time]++;
+      } else {
+          timeCounts[time] = 1;
       }
-      return maxNames;
-    })
+  });
 
-    var yScale = d3.scaleLinear()
-      .domain([0, maxl])
-      .range([dimensions.height-dimensions.margin.bottom,dimensions.margin.top])
+  const svg = d3.select("#times")
+      .attr("width", dimensions.svgWidth)
+      .attr("height", dimensions.svgHeight);
 
-    var colorScale = d3.scaleOrdinal()
-                      .domain(keys)
-                      .range([
-                        "#FF5733",
-                        "#0072B5",
-                        "#D62AD0",
-                        "#34C924",
-                        "#FFAA00",
-                        "#6600CC",
-                        "#4F7942",
-                        "#FF0099"
-                      ])
+  const chartWidth = dimensions.svgWidth - dimensions.margin.left - dimensions.margin.right;
+  const chartHeight = dimensions.svgHeight - dimensions.margin.top - dimensions.margin.bottom;
 
-    var stackedData = d3.stack().keys(keys)(dataset)
+  const xScale = d3.scaleLinear()
+      .domain([0, 23])
+      .range([0, chartWidth]);
 
-    var bars = svg.append("g")
-                  .selectAll("g")
-                  .data(stackedData)
-                  .enter()
-                  .append("g")
-                  .attr("fill", d => colorScale(d.key))
-                  .selectAll("rect")
-                  .data(function(d) {return d})
-                  .enter()
-                  .append("rect")
-                  .attr("x", d => xScale(+d.data.year))
-                  .attr("y", d => yScale(d[1]))
-                  .attr("height", d => yScale(d[0]) - yScale(d[1]))
-                  .attr("width", d => 2)
+  const yScale = d3.scaleLinear()
+      .domain([0, d3.max(Object.values(timeCounts))])
+      .nice()
+      .range([chartHeight, 0]);
 
-    
-    var xAxisGen = d3.axisBottom().scale(xScale)
+  const xAxis = d3.axisBottom(xScale)
+      .tickFormat(d => `${d}:00`);
 
-    var xAxis = svg.append("g")
-        .call(xAxisGen)
-        .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
+  const yAxis = d3.axisLeft(yScale);
 
-    var yAxisGen = d3.axisLeft().scale(yScale)
-    var yAxis = svg.append("g")
-                    .call(yAxisGen)
-                    .style("transform", `translateX(${dimensions.margin.left}px)`)
+  const chart = svg.append("g")
+      .attr("transform", `translate(${dimensions.margin.left},${dimensions.margin.top})`);
 
-  })
-  
+  chart.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(xAxis);
+
+  chart.append("g")
+      .attr("class", "y-axis")
+      .call(yAxis);
+
+  const line = d3.line()
+      .x(d => xScale(d[0]))
+      .y(d => yScale(d[1]));
+
+  const timeData = Object.entries(timeCounts);
+
+  chart.append("path")
+      .datum(timeData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("d", line);
+
+  // Add labels and title
+  svg.append("text")
+      .attr("x", dimensions.svgWidth / 2)
+      .attr("y", dimensions.svgHeight - 20)
+      .style("text-anchor", "middle")
+      .text("Hour of the Day");
+
+  svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -dimensions.svgHeight / 2)
+      .attr("y", 20)
+      .style("text-anchor", "middle")
+      .text("Crash Counts");
+
+  // Add a chart title
+  svg.append("text")
+      .attr("x", dimensions.svgWidth / 2)
+      .attr("y", dimensions.margin.top)
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .text("Crash Counts by Hour of the Day");
+
+});
+
+
