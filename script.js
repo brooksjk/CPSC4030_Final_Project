8c17bbd89264137721c10419003482699cc4924e
evkes <evkessler2002@gmail.com>
2023-11-05 20:44:56 -0500

    Changed file names, integrated bubble

--- script.js
+++ script.js
@@ -60,26 +60,6 @@ d3.csv("cleaned_crash_data_zipc.csv").then(data => {
             .attr("offset", "100%")
             .attr("stop-color", "#013220")
             .attr("stop-opacity", 1);
-
-        svg.append("rect")
-            .attr("x", dimensions.legendX)
-            .attr("y", dimensions.legendY)
-            .attr("width", dimensions.legendWidth)
-            .attr("height", dimensions.legendHeight)
-            .style("fill", "url(#gradient)");
-
-        svg.append("text")
-            .attr("x", dimensions.legendX)
-            .attr("y", dimensions.legendY - 5)
-            .style("font-size", "16px")
-            .text("0");
-
-        svg.append("text")
-            .attr("x", dimensions.legendX + dimensions.legendWidth)
-            .attr("y", dimensions.legendY - 5)
-            .style("font-size", "16px")
-            .attr("text-anchor", "end")
-            .text(maxCount);
     });
 });
 
@@ -97,8 +77,8 @@ d3.csv("cleaned_crash_data_zipc.csv").then(data => {
     };
 
     let attributes = [
-        // "NUMBER OF PERSONS INJURED",
-        // "NUMBER OF PERSONS KILLED",
+        "NUMBER OF PERSONS INJURED",
+        "NUMBER OF PERSONS KILLED",
         "NUMBER OF PEDESTRIANS INJURED",
         "NUMBER OF PEDESTRIANS KILLED",
         "NUMBER OF CYCLIST INJURED",
@@ -207,3 +187,84 @@ d3.csv("cleaned_crash_data_zipc.csv").then(data => {
 });
 
 
+function countContributingFactors(data) {
+    let factorCounts = {};
+
+    data.forEach(row => {
+        let factor = row["CONTRIBUTING FACTOR VEHICLE 1"];
+        if (factor != "none") { 
+            if (factorCounts[factor]) {
+                factorCounts[factor]++;
+            } else {
+                factorCounts[factor] = 1;
+            }
+        }
+    });
+
+    return factorCounts;
+}
+d3.csv("cleaned_crash_data_zipc.csv").then(data => {
+    let factorCounts = countContributingFactors(data);
+    console.log(factorCounts);
+
+    const width = 800;
+    const height = 700;
+
+    const svg = d3.select('#bubbles')
+        .attr('width', width)
+        .attr('height', height);
+
+    let factors = Object.keys(factorCounts).map(key => ({
+        factor: key,
+        count: factorCounts[key]
+    }));
+
+    let maxCount = d3.max(factors, d => d.count);
+
+    let radiusScale = d3.scaleSqrt()
+        .domain([0, maxCount])
+        .range([10, 100]); 
+
+    let colorScale = d3.scaleLinear()
+        .domain([0, maxCount])
+        .range(["lightblue", "steelblue"]);
+
+    const labelThreshold = 5; 
+
+    let simulation = d3.forceSimulation(factors)
+        .force("charge", d3.forceManyBody().strength(15))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("collision", d3.forceCollide().radius(d => radiusScale(d.count) + 1))
+        .on("tick", ticked);
+
+    function ticked() {
+        let bubbles = svg.selectAll("circle")
+            .data(factors, d => d.factor);
+
+        bubbles.enter().append("circle")
+            .attr("r", d => radiusScale(d.count))
+            .attr("fill", d => colorScale(d.count))
+            .merge(bubbles)
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+
+        bubbles.exit().remove();
+
+        let labels = svg.selectAll("text")
+            .data(factors.filter(d => d.count >= labelThreshold), d => d.factor);
+
+        labels.enter().append("text")
+            .text(d => d.factor)
+            .attr("x", d => d.x)
+            .attr("y", d => d.y)
+            .attr("text-anchor", "middle")
+            .attr("alignment-baseline", "middle")
+            .attr("font-size", "10px")
+            .attr("fill", "black") 
+            .merge(labels)
+            .attr("x", d => d.x)
+            .attr("y", d => d.y);
+
+        labels.exit().remove();
+    }
+});
